f913d9fb92f87babef68c43eeee2719d9fced209
unknown <eramirez@SJOERAMIREZ01L.tqs.com>
2014-10-01 11:15:47 -0600

    Initial Commit

---
+++ AppConfigEncriptor/w_main.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using AppConfigEncryptor.BLL;
+
+namespace AppConfigEncriptor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ExtraInitialize();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected void ExtraInitialize()
+        {
+            _fileDialog = new OpenFileDialog();
+            _fileDialog.Filter = ".Net Executables|*.exe";
+            _fileDialog.FileName = "";
+        }
+
+        private void OpenFile()
+        {
+            if (_fileDialog == null) return;
+
+            _fileDialog.ShowDialog();
+
+            _filePath = _fileDialog.FileName;
+
+            txtFileName.Text = _filePath;
+        }
+
+        #endregion
+
+        #region Events
+
+        private void txtFileName_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void btn_selectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void btn_encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var encrypter = new AppConfigEncryptorBlo();
+                encrypter.EncryptFile(_filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btn_decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var encrypter = new AppConfigEncryptorBlo();
+                encrypter.DecryptFile(_filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Attributes
+
+        private OpenFileDialog _fileDialog;
+
+        private string _filePath;
+
+        #endregion
+
+    }
+}
---
+++ AppConfigEncryptor.BLL/AppConfigEncryptorBlo.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace AppConfigEncryptor.BLL
+{
+    /// <summary>
+    /// Based in the code of Preetam U Ramdhave,
+    /// URL http://www.codeproject.com/Tips/598863/EncryptionplusDecryptionplusConnectionplusStringpl
+    /// </summary>
+
+    public class AppConfigEncryptorBlo
+    {
+
+        #region Constructor
+        
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void EncryptFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                EncryptConnectionString(true, fileName);
+            }
+            else
+            {
+                throw new System.IO.FileNotFoundException();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void DecryptFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                EncryptConnectionString(false, fileName);
+            }
+            else
+            {
+                throw new System.IO.FileNotFoundException();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="encrypt"></param>
+        /// <param name="fileName"></param>
+        private  void EncryptConnectionString(bool encrypt, string fileName)
+        {
+            Configuration configuration = null;
+            try
+            {
+                // Open the configuration file and retrieve the connectionStrings section.
+                configuration = ConfigurationManager.OpenExeConfiguration(fileName);
+                ConnectionStringsSection configSection = configuration.GetSection("connectionStrings") as ConnectionStringsSection;
+                if ((!(configSection.ElementInformation.IsLocked)) && (!(configSection.SectionInformation.IsLocked)))
+                {
+                    if (encrypt && !configSection.SectionInformation.IsProtected)
+                    {
+                        //this line will encrypt the file
+                        configSection.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+                    }
+
+                    if (!encrypt && configSection.SectionInformation.IsProtected)//encrypt is true so encrypt
+                    {
+                        //this line will decrypt the file. 
+                        configSection.SectionInformation.UnprotectSection();
+                    }
+                    //re-save the configuration file section
+                    configSection.SectionInformation.ForceSave = true;
+                    // Save the current configuration
+
+                    configuration.Save();
+                    Process.Start("notepad.exe", configuration.FilePath);
+                    //configFile.FilePath 
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Attributes
+        
+        #endregion
+    }
+}
